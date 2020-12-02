935011bc9aee3da77ec30926aa85d9a4e2e39db1
jdavidcarrillo@unicesar.edu.co <jdavidcarrillo@unicesar.edu.co>
2020-12-01 22:31:50 -0500

    commit Proveedor

---
+++ DALL/ColorRepository.cs
@@ -0,0 +1,34 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL
+{
+    class ColorRepository
+    {
+        private readonly OracleConnection _connection;
+        public ColorRepository(ConnectionManager connection)
+        {
+            _connection = connection._conexion;
+        }
+
+
+        public int Guardar(string nombreColor)
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = @"Insert Into color (Nombre) 
+                                      values (:Nombre)";
+                command.Parameters.Add("Nombre", OracleDbType.Varchar2).Value = nombreColor;
+               
+                var filas = command.ExecuteNonQuery();
+                return filas;
+            }
+        }
+
+
+    }
+}
