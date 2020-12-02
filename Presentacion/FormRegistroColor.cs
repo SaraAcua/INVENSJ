0400915590fbf63eea4d2704fbe4dbcc62eeca3c
Sara Katerin Acuña Murcia <saramurcia06@outlook.com>
2020-12-01 23:05:14 -0500

    commit producto

--- Presentacion/FormRegistroColor.cs
+++ Presentacion/FormRegistroColor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -35,10 +36,10 @@ namespace Presentacion
                 this.DialogResult = DialogResult.None;
             }
         }
-        private Color MapearColor()
+        private String MapearColor()
         {
             color = new Color();
-          //  color.Nombre = txtNombre.Text;
+           color. = txtNombre.Text;
         
             return color;
 
