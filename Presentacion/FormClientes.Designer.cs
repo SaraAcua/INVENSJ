02a2de202c88fbf182047b2a08b27b8687f8dac2
Sara Katerin Acuña Murcia <saramurcia06@outlook.com>
2020-12-01 23:35:36 -0500

    ok

--- Presentacion/FormClientes.Designer.cs
+++ Presentacion/FormClientes.Designer.cs
@@ -28,28 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtgcliente = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtId = new Guna.UI2.WinForms.Guna2TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.BtnRegsitrarCliente = new System.Windows.Forms.Button();
             this.btnEditarCliente = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.BtnRegsitrarCliente = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgcliente)).BeginInit();
             this.guna2GradientPanel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dtgcliente
             // 
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(62, 183);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(612, 217);
-            this.dataGridView1.TabIndex = 0;
+            this.dtgcliente.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgcliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgcliente.Location = new System.Drawing.Point(62, 183);
+            this.dtgcliente.Name = "dtgcliente";
+            this.dtgcliente.Size = new System.Drawing.Size(612, 217);
+            this.dtgcliente.TabIndex = 0;
             // 
             // label1
             // 
@@ -67,8 +67,8 @@
             // 
             this.guna2GradientPanel1.BorderRadius = 15;
             this.guna2GradientPanel1.Controls.Add(this.label2);
-            this.guna2GradientPanel1.Controls.Add(this.button2);
-            this.guna2GradientPanel1.Controls.Add(this.guna2TextBox1);
+            this.guna2GradientPanel1.Controls.Add(this.btnBuscar);
+            this.guna2GradientPanel1.Controls.Add(this.txtId);
             this.guna2GradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(66)))), ((int)(((byte)(97)))));
             this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(119)))), ((int)(((byte)(173)))));
             this.guna2GradientPanel1.Location = new System.Drawing.Point(62, 78);
@@ -90,48 +90,49 @@
             this.label2.TabIndex = 19;
             this.label2.Text = "Identificacion";
             // 
-            // button2
+            // btnBuscar
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(119)))), ((int)(((byte)(173)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Image = global::Presentacion.Properties.Resources.buscar;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(139, 39);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 30);
-            this.button2.TabIndex = 18;
-            this.button2.Text = "     Buscar";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnBuscar.BackColor = System.Drawing.Color.Transparent;
+            this.btnBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnBuscar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(119)))), ((int)(((byte)(173)))));
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Image = global::Presentacion.Properties.Resources.buscar;
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(139, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(83, 30);
+            this.btnBuscar.TabIndex = 18;
+            this.btnBuscar.Text = "     Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
-            // guna2TextBox1
+            // txtId
             // 
-            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox1.DefaultText = "";
-            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
-            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
-            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
-            this.guna2TextBox1.Location = new System.Drawing.Point(40, 39);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.guna2TextBox1.Name = "guna2TextBox1";
-            this.guna2TextBox1.PasswordChar = '\0';
-            this.guna2TextBox1.PlaceholderText = "";
-            this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
-            this.guna2TextBox1.Size = new System.Drawing.Size(92, 20);
-            this.guna2TextBox1.TabIndex = 17;
+            this.txtId.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtId.DefaultText = "";
+            this.txtId.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtId.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtId.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtId.DisabledState.Parent = this.txtId;
+            this.txtId.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtId.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtId.FocusedState.Parent = this.txtId;
+            this.txtId.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtId.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtId.HoverState.Parent = this.txtId;
+            this.txtId.Location = new System.Drawing.Point(40, 39);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Name = "txtId";
+            this.txtId.PasswordChar = '\0';
+            this.txtId.PlaceholderText = "";
+            this.txtId.SelectedText = "";
+            this.txtId.ShadowDecoration.Parent = this.txtId;
+            this.txtId.Size = new System.Drawing.Size(92, 20);
+            this.txtId.TabIndex = 17;
             // 
             // panel3
             // 
@@ -141,25 +142,6 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(168, 58);
             this.panel3.TabIndex = 22;
-            // 
-            // BtnRegsitrarCliente
-            // 
-            this.BtnRegsitrarCliente.BackColor = System.Drawing.Color.Transparent;
-            this.BtnRegsitrarCliente.FlatAppearance.BorderSize = 0;
-            this.BtnRegsitrarCliente.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.BtnRegsitrarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnRegsitrarCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnRegsitrarCliente.ForeColor = System.Drawing.Color.Black;
-            this.BtnRegsitrarCliente.Image = global::Presentacion.Properties.Resources.diskette;
-            this.BtnRegsitrarCliente.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.BtnRegsitrarCliente.Location = new System.Drawing.Point(3, 2);
-            this.BtnRegsitrarCliente.Name = "BtnRegsitrarCliente";
-            this.BtnRegsitrarCliente.Size = new System.Drawing.Size(80, 54);
-            this.BtnRegsitrarCliente.TabIndex = 7;
-            this.BtnRegsitrarCliente.Text = "Registrar";
-            this.BtnRegsitrarCliente.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.BtnRegsitrarCliente.UseVisualStyleBackColor = false;
-            this.BtnRegsitrarCliente.Click += new System.EventHandler(this.BtnRegsitrarCliente_Click);
             // 
             // btnEditarCliente
             // 
@@ -180,6 +162,25 @@
             this.btnEditarCliente.UseVisualStyleBackColor = false;
             this.btnEditarCliente.Click += new System.EventHandler(this.btnEditarCliente_Click);
             // 
+            // BtnRegsitrarCliente
+            // 
+            this.BtnRegsitrarCliente.BackColor = System.Drawing.Color.Transparent;
+            this.BtnRegsitrarCliente.FlatAppearance.BorderSize = 0;
+            this.BtnRegsitrarCliente.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.BtnRegsitrarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRegsitrarCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegsitrarCliente.ForeColor = System.Drawing.Color.Black;
+            this.BtnRegsitrarCliente.Image = global::Presentacion.Properties.Resources.diskette;
+            this.BtnRegsitrarCliente.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnRegsitrarCliente.Location = new System.Drawing.Point(3, 2);
+            this.BtnRegsitrarCliente.Name = "BtnRegsitrarCliente";
+            this.BtnRegsitrarCliente.Size = new System.Drawing.Size(80, 54);
+            this.BtnRegsitrarCliente.TabIndex = 7;
+            this.BtnRegsitrarCliente.Text = "Registrar";
+            this.BtnRegsitrarCliente.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BtnRegsitrarCliente.UseVisualStyleBackColor = false;
+            this.BtnRegsitrarCliente.Click += new System.EventHandler(this.BtnRegsitrarCliente_Click);
+            // 
             // FormClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -189,11 +190,11 @@
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.guna2GradientPanel1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtgcliente);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormClientes";
             this.Text = "FormClientes";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgcliente)).EndInit();
             this.guna2GradientPanel1.ResumeLayout(false);
             this.guna2GradientPanel1.PerformLayout();
             this.panel3.ResumeLayout(false);
@@ -204,12 +205,12 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgcliente;
         private System.Windows.Forms.Label label1;
         private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private System.Windows.Forms.Button btnBuscar;
+        private Guna.UI2.WinForms.Guna2TextBox txtId;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button btnEditarCliente;
         private System.Windows.Forms.Button BtnRegsitrarCliente;
--- Presentacion/FormClientes.cs
+++ Presentacion/FormClientes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,8 +13,10 @@ namespace Presentacion
 {
     public partial class FormClientes : Form
     {
+        ClienteService clienteService = new ClienteService();
         public FormClientes()
         {
+            clienteService = new ClienteService(ConfigConnection.connectionString, ConfigConnection.ProviderName);
             InitializeComponent();
         }
 
@@ -28,5 +31,29 @@ namespace Presentacion
             FormGestionCliente gestionCliente = new FormGestionCliente();
             gestionCliente.ShowDialog();
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            ConsultaPersonaRespuesta respuesta = new ConsultaPersonaRespuesta();
+
+         
+                dtgcliente.DataSource = null;
+                respuesta = clienteService.ConsultarTodos();
+                dtgcliente.DataSource = respuesta.Personas;
+                //TxtTotal.Text = clienteService.Totalizar().Cuenta.ToString();
+                //txtId.Text = clienteService.TotalizarTipo("F").Cuenta.ToString();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Debe Seleccionar una opción ", "Busqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            MessageBox.Show(respuesta.Mensaje, "Busqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
     }
 }
