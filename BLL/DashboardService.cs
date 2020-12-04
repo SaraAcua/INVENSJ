6d28f89a4e95f76e0d14fcc1ff86253e101ae09a
jdavidcarrillo@unicesar.edu.co <jdavidcarrillo@unicesar.edu.co>
2020-12-04 17:04:57 -0500

    commit

---
+++ BLL/DashboardService.cs
@@ -0,0 +1,45 @@
+﻿using DALL;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class DashboardService
+    {
+
+        private readonly ConnectionManager conexion;
+        private readonly DashboardRepository repositorio;
+        public DashboardService(string connectionString)
+        {
+            conexion = new ConnectionManager(connectionString);
+            repositorio = new DashboardRepository(conexion);
+        }
+
+       
+        
+        public ArrayList ConsultarDatos()
+        {
+            ArrayList Datos = new ArrayList();
+            try
+            {
+                conexion.Open();
+                Datos = repositorio.ConsultarDatos();
+                conexion.Close();
+                
+            }
+            catch
+            {
+
+            }
+            finally { conexion.Close(); }
+            return Datos;
+        }
+
+
+
+    }
+}
---
+++ DALL/DashboardRepository.cs
@@ -0,0 +1,44 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL
+{
+    public class DashboardRepository
+    {
+        private readonly OracleConnection _connection;
+        private readonly ArrayList Datos = new ArrayList();
+        public DashboardRepository(ConnectionManager connection)
+        {
+            _connection = connection._conexion;
+        }
+
+        public ArrayList ConsultarDatos()
+        {
+            OracleDataReader dataReader;
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = @"DashboardDatos";
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                dataReader = command.ExecuteReader();
+                dataReader.Read();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        Datos.Add ( dataReader.GetInt32(0));
+
+                    }
+                }
+            }
+            return Datos;
+        }
+
+
+
+    }
+}
