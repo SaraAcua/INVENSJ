19316ddeb14d7294e583dc3d9ecfdcb75d3ed908
jdavidcarrillo@unicesar.edu.co <jdavidcarrillo@unicesar.edu.co>
2020-12-05 22:07:49 -0500

    Commit

--- ENTITY/Movimientos.cs
+++ ENTITY/Movimientos.cs
@@ -16,20 +16,22 @@ namespace ENTITY
             Codigo = producto.CodigoProducto;
             Cantidad = cantidad;
             Fecha = fecha;
-            Tipo = "Movimiento";
+            
         }
 
         public Movimientos()
         {
 
         }
-        public string Descripcion { get; set; }
         public string Codigo { get; set; }
         public string Motivo { get; set; }
-        public Producto Producto { get; set; }
+        public string Descripcion { get; set; }
+        
+        
+        
         public int Cantidad { get; set; }
         public DateTime Fecha { get; set; }
-        public string Tipo { get; set; }
+        
 
 
     }
