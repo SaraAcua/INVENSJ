b69f5760d5f1941b473378f3a9d8af555c46022e
jdavidcarrillo@unicesar.edu.co <jdavidcarrillo@unicesar.edu.co>
2020-12-05 22:22:48 -0500

    oki

--- ENTITY/Cliente.cs
+++ ENTITY/Cliente.cs
@@ -27,12 +27,13 @@ namespace ENTITY
 
         }
 
-        public string Barrio { get; set; }
+        
         public string TipoIdentificacion { get; set; }
         public string Identificacion { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string Direccion { get; set; }
+        public string Barrio { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
         
